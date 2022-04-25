6383c5aa1fbd413401ca4dd05eab363b4e845f9d
parksunmi1 <bfsunrise@hotmail.com>
2022-04-25 16:52:15 +0900

    첫번째 클래스를 연습

---
+++ class1.py
@@ -0,0 +1,15 @@
+# class1.py
+#1)클래스를 정의
+class Person:
+    #초기화 메서드
+    def __init__(self):
+        self.name = "default name"
+    def print(self):
+        print("My name is {0}".format(selt.name))
+
+#2)인스턴스 생성
+p1 = Person()
+#3)메스드 호출
+p1.print()
+
+        
